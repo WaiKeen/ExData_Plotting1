123f5d43a535e6866e901bd88ad6eeba6c0394b0
WaiKeen <ho.waikeen@vads.com>
2015-07-09 16:29:59 +0800

    Exploratoy Data Analysis - Course Project 1

--- plot3.R
+++ plot3.R
@@ -6,7 +6,7 @@ powersubset <- power[power$Date %in% c("1/2/2007","2/2/2007"),]
 datetime <- strptime(paste(powersubset$Date, powersubset$Time),format = "%d/%m/%Y %H:%M")
 
 #Open PNG Device, create "plot1.png" with width & height at 480 pixels.
-#Create Line Plot for Date&Time against Global Active Power from subset data of 1st & 2nd Feb 2007 agains
+#Create Line Plot for Date&Time against Sub Metering with Legend
 #Close the PNG file device
 png("plot3.png",width = 480, height = 480)
 
